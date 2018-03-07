429fb336994c2544e832b2a2ce6101ce5e5af745
Yuehan Duan <yuehanduan@yahoo.com>
2018-03-07 12:08:09 -0800

    Render all elements on proxy server.

--- proxy/router.js
+++ proxy/router.js
@@ -1,11 +1,50 @@
 const express = require("express");
 const router = express.Router();
-const Review = require("./../feedback/database/index.js");
+const axios = require("axios");
 
 router.get("/feedback", function(req, res) {
-  Review.Review.find({}).then(results => {
-    res.send(results);
-  });
+  axios
+    .get("http://localhost:3002/feedback")
+    .then(results => res.send(results.data))
+    .catch(err => {
+      console.log(err);
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+    });
+});
+
+router.get("/api/compare", function(req, res) {
+  let id = req.query;
+  console.log("REQUEST ID: ", id);
+  axios
+    .get("http://localhost:3004/api/compare", { params: id })
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+    });
+});
+
+router.get("/description", function(req, res) {
+  axios
+    .get("http://localhost:3001/description")
+    .then(results => res.send(results.data))
+    .catch(err => {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+    });
+});
+
+router.get("/load", function(req, res) {
+  axios
+    .get("http://localhost:3001/load")
+    .then(results => res.send(results.data))
+    .catch(err => {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+    });
 });
 
 module.exports = router;
